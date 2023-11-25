f3f5d298f91c77f17bda526e9d579627e027b49f
jhoanboniilla <146584620+jhoanboniilla@users.noreply.github.com>
2023-11-24 22:24:05 -0400

    Add files via upload

---
+++ generar un numero aleatorio entre 1-100.py
@@ -0,0 +1,6 @@
+"generar un numero aleatorio entre 1-100"
+import random
+
+numero = random.randint(1, 100)
+
+print (numero)
---
+++ generator un numero aleatorio.py
@@ -0,0 +1,7 @@
+"Generar un numero aleatorio"
+
+import random
+
+numero= random.random()
+
+print(numero)
